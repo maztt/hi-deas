6221d6dcda0be98a95aa4d8d3c22e4c6faa35995
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-01 15:21:21 -0300

    feat: add Auth Controller user login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -8,6 +8,34 @@ module.exports = class AuthController {
     res.render('auth/login')
   }
 
+  static async loginPost (req, res) {
+    const {email, password} = req.body
+
+    const user = await User.findOne({where: {email: email}})
+
+    if (!user) {
+      req.flash('message', 'This user does not exist!')
+      res.render('auth/login')
+
+      return
+    }
+
+    const checkIfPasswordMatches = bcrypt.compareSync(password, user.password)
+
+    if (!checkIfPasswordMatches) {
+      req.flash('message', 'Password is invalid!')
+      res.render('auth/login')
+
+      return
+    }
+
+    req.session.userId = user.id
+
+    req.session.save(() => {
+      res.redirect('/')
+    })
+  }
+
   static register (req, res) {
     res.render('auth/register')
   }

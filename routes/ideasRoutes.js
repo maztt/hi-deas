8d690ac623eee2a1fced234d004f1a0bcfdfe974
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-01 15:30:34 -0300

    feat: add Middleware to Auth Routes(dashboard)

--- routes/ideasRoutes.js
+++ routes/ideasRoutes.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const IdeaController = require('../controllers/IdeaController')
 
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/dashboard', checkAuth, IdeaController.dashboard)
 router.get('/', IdeaController.showAll)
 
 module.exports = router

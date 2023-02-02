1a7c85c0287ea9327dd96c7e6e7237c8712410e2
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-02 16:17:05 -0300

    feat: add Idea Routes(newIdeaPost)

--- routes/ideasRoutes.js
+++ routes/ideasRoutes.js
@@ -5,6 +5,7 @@ const IdeaController = require('../controllers/IdeaController')
 const checkAuth = require('../helpers/auth').checkAuth
 
 router.get('/new', checkAuth, IdeaController.newIdea)
+router.post('/new', checkAuth, IdeaController.newIdeaPost)
 router.get('/dashboard', checkAuth, IdeaController.dashboard)
 router.get('/', IdeaController.showAll)
 

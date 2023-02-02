2272498a9df0106c494ceabf5124cb96a6c1973e
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-02 16:15:17 -0300

    feat: add Idea Controller posting idea

--- controllers/IdeaController.js
+++ controllers/IdeaController.js
@@ -14,4 +14,24 @@ module.exports = class IdeaController {
   static newIdea (req, res) {
     res.render('ideas/new')
   }
+
+  static async newIdeaPost (req, res) {
+    const idea = {
+      title: req.body.title,
+      description: req.body.description,
+      UserId: req.session.userId
+    }
+    
+    try {
+      await Idea.create(idea)
+  
+      req.flash('message', 'You just sent your idea! Now wait for the start up investors!')
+      
+      req.session.save(() => {
+        res.redirect('/ideas/dashboard')
+      })
+    } catch(err) {
+      console.error(`An error occurred: ${err}`)
+    }
+  }
 }

aca9576d4360f7ab2eb1e0fa0e64916d1e48ce5a
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-08-24 15:35:16 -0300

    feat(models/Idea): add id as a new field for the schema

--- models/Idea.js
+++ models/Idea.js
@@ -3,6 +3,12 @@ import db from '../db/conn.js'
 import User from './User.js'
 
 const Idea = db.define('Idea', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
     title: {
         type: DataTypes.STRING,
         allowNull: false,
